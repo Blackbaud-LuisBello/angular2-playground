6e3f69315907b3d002bf9fdeaa40f5c131d6f8f6
Luis Bello <luis.bello@blackbaud.com>
2016-06-14 21:26:20 -0400

    more routing stuff

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,6 +2,7 @@ import { Component }       from '@angular/core';
 import { HeroService }     from './hero.service';
 import { HeroesComponent } from './heroes.component';
 import { DashboardComponent } from './dashboard.component';
+import { HeroDetailComponent } from './hero-detail.component';
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/router-deprecated';
 
 @Component({
@@ -32,6 +33,11 @@ import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from '@angular/route
     name: 'Dashboard',
     component: DashboardComponent,
     useAsDefault: true
+  },
+  {
+    path: '/detail/:id',
+    name: 'HeroDetail',
+    component: HeroDetailComponent
   }
 ])
 
